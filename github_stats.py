280abae35cc40da0d5170a210bb327c4d057c906
pakagronglb <pakagronglebel@gmail.com>
2025-01-28 00:58:44 +0700

    feat: add programming language distribution visualization

--- github_stats.py
+++ github_stats.py
@@ -1,25 +1,40 @@
 import requests
 
-def fetch_contribution_data(username, token):
+def fetch_contribution_data(username: str, token: str) -> dict:
+    """Fetch GitHub contribution data for a user."""
     url = "https://api.github.com/graphql"
     headers = {"Authorization": f"Bearer {token}"}
-    query = f"""
-    {{
-      user(login: "{username}") {{
-        contributionsCollection {{
-          contributionCalendar {{
-            totalContributions
-            weeks {{
-              contributionDays {{
-                date
-                contributionCount
-              }}
-            }}
-          }}
-        }}
-      }}
-    }}
-    """
+    query = """
+    {
+        user(login: "%s") {
+            name
+            contributionsCollection {
+                totalCommitContributions
+                restrictedContributionsCount
+                contributionCalendar {
+                    totalContributions
+                    weeks {
+                        contributionDays {
+                            contributionCount
+                            date
+                        }
+                    }
+                }
+            }
+            repositories(first: 100, ownerAffiliations: OWNER, isFork: false) {
+                edges {
+                    node {
+                        name
+                        primaryLanguage {
+                            name
+                            color
+                        }
+                    }
+                }
+            }
+        }
+    }
+    """ % username
     try:
         response = requests.post(url, json={"query": query}, headers=headers)
         response.raise_for_status()  # Raises HTTPError if the response was unsuccessful
@@ -54,3 +69,19 @@ def process_contribution_data(data):
         }
     except KeyError:
         return {"errors": "Invalid data structure"}
+
+def process_language_data(data: dict) -> dict:
+    """Process GitHub data to get language statistics."""
+    if not data or 'data' not in data or 'user' not in data['data']:
+        return {}
+    
+    repositories = data['data']['user']['repositories']['edges']
+    language_count = {}
+
+    for repo in repositories:
+        language = repo['node'].get('primaryLanguage')
+        if language and language.get('name'):
+            language_name = language['name']
+            language_count[language_name] = language_count.get(language_name, 0) + 1
+
+    return language_count
